ddf4ae657afe8ea15150267d60870e1aaa53ad3a
Roy <royparra12@gmail.com>
2019-07-24 20:13:14 +0200

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,34 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,public alertController: AlertController) {}
 
   enterList(){
     this.router.navigateByUrl("/list");
+  }
+  async AlertHome() {
+    const alert = await this.alertController.create({
+      header: 'Ver trámites',
+      message: this.bodyHtml(),
+      buttons: [{
+        text: 'Entrar',
+        handler: () => {
+          this.router.navigateByUrl("/list");
+        }
+      },
+      {
+        text: 'Cerrar',
+        role: 'cancel',
+        cssClass: 'secondary',
+      }]
+    });
+
+    await alert.present();
+  }
+  bodyHtml(){
+    var body = '<center><img src="./assets/img/unfv-logo.jpg"></center>'
+    body += '<p>A continuacion se mostraran la lista de tramites de pregrado de la UNFV</p>'
+    return body
   }
 }
 
